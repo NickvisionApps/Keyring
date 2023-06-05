0cfd733b9651c902d2874645d4323f37333dddb2
Nick Logozzo <nlogozzo225@gmail.com>
2023-06-05 18:38:08 -0400

    Implement Keyring API

--- Nickvision.Keyring/Keyring.cs
+++ Nickvision.Keyring/Keyring.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Nickvision.Keyring;
 
@@ -30,4 +32,39 @@ public class Keyring
             _store = Store.Create(name, password, true);
         }
     }
+
+    /// <summary>
+    /// Lookups a credential by id
+    /// </summary>
+    /// <param name="id">The id of the credential</param>
+    /// <returns>The Credential object if found, else null</returns>
+    public async Task<Credential?> LookupCredentialAsync(int id) => await _store.LookupCredentialAsync(id);
+
+    /// <summary>
+    /// Lookups credentials by name
+    /// </summary>
+    /// <param name="name">The name of the credentials to find</param>
+    /// <returns>The list of Credential objects found</returns>
+    public async Task<List<Credential>> LookupCredentialsAsync(string name) => await _store.LookupCredentialsAsync(name);
+
+    /// <summary>
+    /// Adds a Credential to the Keyring
+    /// </summary>
+    /// <param name="credential">The Credential object to add</param>
+    /// <returns>True if successful, else false</returns>
+    public async Task<bool> AddCredentialAsync(Credential credential) => await _store.AddCredentialAsync(credential);
+
+    /// <summary>
+    /// Updates a Credential in the Keyring
+    /// </summary>
+    /// <param name="credential">The Credential object to update</param>
+    /// <returns>True if successful, else false</returns>
+    public async Task<bool> UpdateCredentialAsync(Credential credential) => await _store.UpdateCredentialAsync(credential);
+
+    /// <summary>
+    /// Removes a Credential from the Keyring
+    /// </summary>
+    /// <param name="id">The id of the credential to remove</param>
+    /// <returns>True if successful, else false</returns>
+    public async Task<bool> DeleteCredentialAsync(int id) => await _store.DeleteCredentialAsync(id);
 }
