fc49d8d8b2ba688b55223ba385a1a5dde10a0512
Nick Logozzo <nlogozzo225@gmail.com>
2023-06-05 17:55:03 -0400

    Update Credential.cs

--- Nickvision.Keyring/Credential.cs
+++ Nickvision.Keyring/Credential.cs
@@ -5,7 +5,7 @@ namespace Nickvision.Keyring;
 /// <summary>
 /// A model of a credential stored in the keyring
 /// </summary>    
-public class Credential
+public class Credential : IComparable<Credential>, IEquatable<Credential>
 {
     /// <summary>
     /// The id of the credential
@@ -60,4 +60,89 @@ public class Credential
         Username = username;
         Password = password;
     }
+
+    /// <summary>
+    /// Compares this with other
+    /// </summary>
+    /// <param name="other">The Credential object to compare to</param>
+    /// <returns>-1 if this is less than other. 0 if this is equal to other. 1 if this is greater than other</returns>
+    /// <exception cref="NullReferenceException">Thrown if other is null</exception>
+    public int CompareTo(Credential? other)
+    {
+        if (other == null)
+        {
+            throw new NullReferenceException();
+        }
+        if (this < other)
+        {
+            return -1;
+        }
+        else if (this == other)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    /// <summary>
+    /// Gets whether or not an object is equal to this Credential
+    /// </summary>
+    /// <param name="obj">The object to compare</param>
+    /// <returns>True if equals, else false</returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj is Credential toCompare)
+        {
+            return Id == toCompare.Id;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Gets whether or not an object is equal to this Credential
+    /// </summary>
+    /// <param name="obj">The Credential? object to compare</param>
+    /// <returns>True if equals, else false</returns>
+    public bool Equals(Credential? obj) => Equals((object?)obj);
+
+    /// <summary>
+    /// Gets a hash code for the object
+    /// </summary>
+    /// <returns>The hash code for the object</returns>
+    public override int GetHashCode() => Id.GetHashCode();
+
+    /// <summary>
+    /// Compares two Credential objects by ==
+    /// </summary>
+    /// <param name="a">The first Credential object</param>
+    /// <param name="b">The second Credential object</param>
+    /// <returns>True if a == b, else false</returns>
+    public static bool operator ==(Credential? a, Credential? b) => a?.Id == b?.Id;
+
+    /// <summary>
+    /// Compares two Credential objects by !=
+    /// </summary>
+    /// <param name="a">The first Credential object</param>
+    /// <param name="b">The second Credential object</param>
+    /// <returns>True if a != b, else false</returns>
+    public static bool operator !=(Credential? a, Credential? b) => a?.Id != b?.Id;
+
+    /// <summary>
+    /// Compares two Credential objects by >
+    /// </summary>
+    /// <param name="a">The first Credential object</param>
+    /// <param name="b">The second Credential object</param>
+    /// <returns>True if a > b, else false</returns>
+    public static bool operator <(Credential? a, Credential? b) => a?.Id < b?.Id;
+
+    /// <summary>
+    /// Compares two Credential objects by <
+    /// </summary>
+    /// <param name="a">The first Credential object</param>
+    /// <param name="b">The second Credential object</param>
+    /// <returns>True if a < b, else false</returns>
+    public static bool operator >(Credential? a, Credential? b) => a?.Id > b?.Id;
 }
