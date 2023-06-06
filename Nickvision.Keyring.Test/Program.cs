93955da034c8cf2c69b6eee2a2eeba4034ca489c
Nick Logozzo <nlogozzo225@gmail.com>
2023-06-05 20:05:27 -0400

    Test - Some More Implementation

--- Nickvision.Keyring.Test/Program.cs
+++ Nickvision.Keyring.Test/Program.cs
@@ -1,45 +1,54 @@
 ﻿using Nickvision.Keyring;
 using System;
+using System.Threading.Tasks;
 
 namespace Nickvision.Keyring.Test;
 
 public class Program
 {
-    public static void Main(string[] args) => new Program().MainView();
+    public static async Task Main(string[] args) => await new Program().MainViewAsync();
     
-    private void MainView()
+    private async Task MainViewAsync()
     {
         var isRunning = true;
         while(isRunning)
         {
             Console.Clear();
             Console.WriteLine("===Nickvision.Keyring Testing Program===");
-            Console.WriteLine("1. Create/Load a Keyring");
-            Console.WriteLine("2. Exit");
+            Console.WriteLine("1. Access a keyring");
+            Console.WriteLine("2. Check if keyring exists");
+            Console.WriteLine("3. Exit");
             Console.Write("Select an Option: ");
             var input = Console.ReadLine();
             if(input == "1")
             {
-                Console.WriteLine("\n=Keyring Login=");
-                Console.Write("Name: ");
+                Console.Write("\nName: ");
                 var name = Console.ReadLine();
                 Console.Write("Password: ");
                 var password = Console.ReadLine();
-                KeyringView(name, password);
+                await KeyringViewAsync(name, password);
             }
             else if(input == "2")
+            {
+                Console.Write("\nName: ");
+                var name = Console.ReadLine();
+                Console.WriteLine(Keyring.Exists(name) ? "The keyring exists." : "The keyring does not exist.");
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+            }
+            else if(input == "3")
             {
                 isRunning = false;
             }
         }   
     }
     
-    private void KeyringView(string name, string password)
+    private async Task KeyringViewAsync(string name, string password)
     {
         var keyring = Keyring.Access(name, password);
         if(keyring == null)
         {
-            Console.WriteLine("\nUnable to access the keyring.\nPress any key to continue...");
+            Console.WriteLine("Unable to access the keyring.\n\nPress any key to continue...");
             Console.ReadKey();
             return;
         }
@@ -58,7 +67,15 @@ public class Program
             var input = Console.ReadLine();
             if(input == "1")
             {
-                
+                var credentials = await keyring.GetAllCredentialsAsync();
+                Console.Clear();
+                Console.WriteLine($"===Credentials in {keyring.Name}===");
+                foreach(var credential in credentials)
+                {
+                    Console.WriteLine($"{credential.Name}: {credential.Id} => {credential.Uri} = {credential.Username} | {credential.Password}");
+                }
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
             }
             else if(input == "2")
             {
