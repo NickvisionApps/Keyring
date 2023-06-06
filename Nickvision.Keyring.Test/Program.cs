0e9468fb4a40a68398fada2123331f4d34f9aad8
Nick Logozzo <nlogozzo225@gmail.com>
2023-06-05 20:26:21 -0400

    Test - Finish Implementation

--- Nickvision.Keyring.Test/Program.cs
+++ Nickvision.Keyring.Test/Program.cs
@@ -16,6 +16,7 @@ public class Program
             Console.Clear();
             Console.WriteLine("===Nickvision.Keyring Testing Program===");
             Console.WriteLine("1. Access a keyring");
+            Console.WriteLine("\t-> Creates a keyring if one with the name doesn't exist, otherwise logs in to the keyring with the provided name");
             Console.WriteLine("2. Check if keyring exists");
             Console.WriteLine("3. Exit");
             Console.Write("Select an Option: ");
@@ -79,15 +80,130 @@ public class Program
             }
             else if(input == "2")
             {
-
+                Console.Write("\nName (Required): ");
+                var credentialName = Console.ReadLine();
+                while(string.IsNullOrEmpty(credentialName))
+                {
+                    Console.Write("Name (Required): ");
+                    credentialName = Console.ReadLine();
+                }
+                Console.Write("Uri (Optional): ");
+                var credentialUri = Console.ReadLine();
+                Uri? credentialUriReal = null;
+                if(!string.IsNullOrEmpty(credentialUri))
+                {
+                    try
+                    {
+                        credentialUriReal = new Uri(credentialUri);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Uri format invalid. Skipping...");
+                    }
+                }
+                Console.Write("Username (Optional): ");
+                var credentialUsername = Console.ReadLine();
+                Console.Write("Password (Optional): ");
+                var credentialPassword = Console.ReadLine();
+                await keyring.AddCredentialAsync(new Credential(credentialName, credentialUriReal, credentialUsername, credentialPassword));
             }
             else if(input == "3")
             {
-
+                Console.Write("\nId: ");
+                var id = Console.ReadLine();
+                int? idReal = null;
+                if(!string.IsNullOrEmpty(id))
+                {
+                    try
+                    {
+                        idReal = int.Parse(id);
+                    }
+                    catch { }
+                }
+                if(idReal == null)
+                {
+                    Console.WriteLine("No credential found with the provided id.\n\nPress any key to continue...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Credential? credential = await keyring.LookupCredentialAsync(idReal.Value);
+                    if(credential == null)
+                    {
+                        Console.WriteLine("No credential found with the provided id.\n\nPress any key to continue...");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.Write("\nNew Name (Required): ");
+                        var credentialName = Console.ReadLine();
+                        while(string.IsNullOrEmpty(credentialName))
+                        {
+                            Console.Write("New Name (Required): ");
+                            credentialName = Console.ReadLine();
+                        }
+                        Console.Write("New Uri (Optional): ");
+                        var credentialUri = Console.ReadLine();
+                        Uri? credentialUriReal = null;
+                        if(!string.IsNullOrEmpty(credentialUri))
+                        {
+                            try
+                            {
+                                credentialUriReal = new Uri(credentialUri);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Uri format invalid. Skipping...");
+                            }
+                        }
+                        Console.Write("New Username (Optional): ");
+                        var credentialUsername = Console.ReadLine();
+                        Console.Write("New Password (Optional): ");
+                        var credentialPassword = Console.ReadLine();
+                        credential.Name = credentialName;
+                        credential.Uri = credentialUriReal;
+                        credential.Username = credentialUsername;
+                        credential.Password = credentialPassword;
+                        await keyring.UpdateCredentialAsync(credential);
+                    }
+                }
             }
             else if(input == "4")
             {
-
+                Console.Write("\nId: ");
+                var id = Console.ReadLine();
+                int? idReal = null;
+                if(!string.IsNullOrEmpty(id))
+                {
+                    try
+                    {
+                        idReal = int.Parse(id);
+                    }
+                    catch { }
+                }
+                if(idReal == null)
+                {
+                    Console.WriteLine("No credential found with the provided id.\n\nPress any key to continue...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Credential? credential = await keyring.LookupCredentialAsync(idReal.Value);
+                    if(credential == null)
+                    {
+                        Console.WriteLine("No credential found with the provided id.\n\nPress any key to continue...");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.Write($"Are you sure you want to delete credential: {credential.Name}? (y/n) ");
+                        var response = Console.ReadLine().ToLower();
+                        if(response == "y" || response == "yes")
+                        {
+                            await keyring.DeleteCredentialAsync(credential.Id);
+                        }
+                    }
+                }
             }
             else if(input == "5")
             {
